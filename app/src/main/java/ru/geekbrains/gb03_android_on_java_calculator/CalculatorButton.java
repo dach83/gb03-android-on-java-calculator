8ba2cb103854d422084d54330fe1845710e64172
dach83 <dach83@mail.ru>
2021-12-21 11:03:41 +0300

    add CalculatorButton enum

---
+++ app/src/main/java/ru/geekbrains/gb03_android_on_java_calculator/CalculatorButton.java
@@ -0,0 +1,43 @@
+package ru.geekbrains.gb03_android_on_java_calculator;
+
+public enum CalculatorButton {
+    NUM_0(R.id.num0_button, "0"),
+    NUM_1(R.id.num1_button, "1"),
+    NUM_2(R.id.num2_button, "2"),
+    NUM_3(R.id.num3_button, "3"),
+    NUM_4(R.id.num4_button, "4"),
+    NUM_5(R.id.num5_button, "5"),
+    NUM_6(R.id.num6_button, "6"),
+    NUM_7(R.id.num7_button, "7"),
+    NUM_8(R.id.num8_button, "8"),
+    NUM_9(R.id.num9_button, "9"),
+    NUM_00(R.id.num00_button, "00"),
+    NUM_DOT(R.id.dot_button, ","),
+    EMPTY_ACTION(0, "");
+
+    private final int id;
+    private final String text;
+
+    CalculatorButton(int id, String name) {
+        this.id = id;
+        this.text = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public static CalculatorButton findById(int id) {
+        // кнопок не много, поэтому для поиска использую простой перебор
+        for (CalculatorButton button : CalculatorButton.values()) {
+            if (button.id == id)
+                return button;
+        }
+        return EMPTY_ACTION;
+    }
+}
+
--- app/src/main/java/ru/geekbrains/gb03_android_on_java_calculator/MainActivity.java
+++ app/src/main/java/ru/geekbrains/gb03_android_on_java_calculator/MainActivity.java
@@ -1,14 +1,37 @@
 package ru.geekbrains.gb03_android_on_java_calculator;
 
+import static ru.geekbrains.gb03_android_on_java_calculator.CalculatorButton.*;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+import java.util.EnumSet;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView inputTextView;
+
+    // множество кнопок предназначенных для ввода числа
+    private final EnumSet<CalculatorButton> numButtons = EnumSet.range(NUM_0, NUM_DOT);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewByIdAndInitListeners();
     }
+
+    private void findViewByIdAndInitListeners() {
+        inputTextView = findViewById(R.id.input_text_view);
+
+        for (CalculatorButton numButton : numButtons) {
+            findViewById(numButton.getId()).setOnClickListener(v -> {
+                CalculatorButton clickedButton = CalculatorButton.findById(v.getId());
+                numButtonClick(clickedButton);
+            });
+        }
+    }
+
+    private void numButtonClick(CalculatorButton button) {
+    }
+
 }
